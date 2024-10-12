d06e7881cebf87fb7674bd9e4234c285b5db1e62
Alejandro <aperalva@gmail.com>
2024-10-12 11:25:21 +0100

    2.11: La Agenda Telefónica Paso 6

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,18 +1,18 @@
 const Persons = ({ persons }) => {
-    return (
-      <div>
-        {persons.map(person => 
-          <Person key={person.id} person={person} />
-        )}
-      </div>
-    )
-  }
-  
-  const Person = ({ person }) => {
-    return (
-      <p>{person.name} {person.number}</p>
-    )
-  }
-  
-  export default Persons
-  
+  return (
+    <div>
+      {persons && persons.length > 0 
+        ? (persons.map(person => <Person key={person.id} person={person} />)) 
+        : (<p>No persons available</p>
+      )}
+    </div>
+  );
+}
+
+const Person = ({ person }) => {
+  return (
+    <p>{person.name} {person.number}</p>
+  );
+}
+
+export default Persons;
