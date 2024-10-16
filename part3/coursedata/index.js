13ce843362f7d9e0d75f36e38b71fbed301b1202
Alejandro <aperalva@gmail.com>
2024-10-16 14:59:59 +0100

    restart

--- part3/coursedata/index.js
+++ part3/coursedata/index.js
@@ -1,11 +1,7 @@
 const express = require('express')
-const cors = require('cors')
-
 const app = express()
 
 app.use(express.json())
-app.use(cors())
-app.use(express.static('dist'))
 
 let notes = [
     {
@@ -77,7 +73,7 @@ app.delete('/api/notes/:id', (request, response) => {
     response.json(note)
   })
 
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
