57974fd98d3813e8fbdbbc547229521ab76cc512
Alejandro <aperalva@gmail.com>
2024-10-20 10:13:54 +0100

    primera lectura parte d P3

--- part3/index.js
+++ part3/index.js
@@ -58,7 +58,7 @@ app.get('/api/notes/:id', (request, response, next) => {
     return maxId + 1
   }
   
-  app.post('/api/notes', (request, response) => {
+  app.post('/api/notes', (request, response, next) => {
     const body = request.body
   
     if (body.content === undefined) {
@@ -70,9 +70,11 @@ app.get('/api/notes/:id', (request, response, next) => {
       important: body.important || false,
     })
   
-    note.save().then(savedNote => {
+    note.save()
+      .then(savedNote => {
       response.json(savedNote)
-    })
+      })
+      .catch(error => next(error))
   })
 
   app.delete('/api/notes/:id', (request, response, next) => {
@@ -84,14 +86,13 @@ app.get('/api/notes/:id', (request, response, next) => {
   })
 
   app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+    const { content, important } = request.body
   
-    const note = {
-      content: body.content,
-      important: body.important,
-    }
-  
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(
+      request.params.id, 
+      { content, important },
+      { new: true, runValidators: true, context: 'query' }
+    )
       .then(updatedNote => {
         response.json(updatedNote)
       })
@@ -109,6 +110,8 @@ app.get('/api/notes/:id', (request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
     } 
   
     next(error)
