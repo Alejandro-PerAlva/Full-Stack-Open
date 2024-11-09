5436abff9f3f43e81fbc8b6507274df4632a0aab
Alejandro <aperalva@gmail.com>
2024-11-09 10:03:27 +0000

    5.18: Pruebas de Extremo a Extremo de la Lista de Blogs, paso 2

--- part5/E2E-blogApp/tests/login.spec.js
+++ part5/E2E-blogApp/tests/login.spec.js
@@ -2,9 +2,21 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('/')
-  })
+    beforeEach(async ({ page, request }) => {
+        await request.post('/api/testing/reset')
+
+        const newUser = {
+          username: 'testuser',
+          name: 'Test User',
+          password: 'testpassword'
+        }
+    
+        await request.post('/api/users', {
+          data: newUser
+        })
+
+        await page.goto('/')
+      })
 
   test('Login form is shown by default', async ({ page }) => {
 
@@ -18,5 +30,30 @@ describe('Blog app', () => {
 
     await expect(usernameInput).toBeVisible()
     await expect(passwordInput).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.click('button:has-text("Log in")')
+
+      await page.fill('input[type="text"]', 'testuser')
+      await page.fill('input[type="password"]', 'testpassword')
+      await page.click('button[type="submit"]')
+
+      const successMessage = await page.locator('text=Logged in as Test User')
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.click('button:has-text("Log in")')
+
+      await page.fill('input[type="text"]', 'testuser')
+      await page.fill('input[type="password"]', 'wrongpassword')
+      await page.click('button[type="submit"]')
+
+      const errorMessage = await page.locator('text=Invalid username or password')
+
+      const loginForm = await page.locator('form')
+      await expect(loginForm).toBeVisible()
+    })
   })
 })
