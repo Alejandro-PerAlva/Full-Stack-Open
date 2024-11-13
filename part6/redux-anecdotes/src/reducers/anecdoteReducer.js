c68326b002e38544ed967a48451024f85d5eb0e5
Alejandro <aperalva@gmail.com>
2024-11-13 09:28:57 +0000

    6.8: Anécdotas, paso 6

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -30,9 +30,6 @@ export const createAnecdote = (content) => ({
 
 // Reducer
 const anecdoteReducer = (state = initialState, action) => {
-  console.log('state now:', state)
-  console.log('action:', action)
-
   switch (action.type) {
     case 'VOTE':
       return state
