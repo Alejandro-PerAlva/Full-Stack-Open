2097805d419a21ea1bbea6a090da298fef82cbb3
Alejandro <aperalva@gmail.com>
2024-11-15 19:25:44 +0000

    6.9 Mejores Anécdotas, paso 7

---
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -0,0 +1,16 @@
+export const setFilter = (filter) => ({
+    type: 'SET_FILTER',
+    payload: filter,
+  })
+  
+  const filterReducer = (state = '', action) => {
+    switch (action.type) {
+      case 'SET_FILTER':
+        return action.payload
+      default:
+        return state
+    }
+  }
+  
+  export default filterReducer
+  
