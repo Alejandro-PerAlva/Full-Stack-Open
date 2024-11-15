737e14ca70130950393018cc5f802c2f7258e918
Alejandro <aperalva@gmail.com>
2024-11-15 19:48:31 +0000

    6.12 Mejores Anécdotas, paso 10

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,29 +1,35 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import { setNotification } from '../reducers/notificationReducer'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const handleSubmit = (event) => {
+  const addAnecdote = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
-    if (content.trim() === '') return
     event.target.anecdote.value = ''
+
     const newAnecdote = {
       content,
-      id: (100000 * Math.random()).toFixed(0),
+      id: Math.random().toFixed(0),
       votes: 0
     }
+
     dispatch(createAnecdote(newAnecdote))
+    dispatch(setNotification(`You added '${content}'`)) 
+
+    setTimeout(() => {
+      dispatch(setNotification(''))
+    }, 5000)
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Create new anecdote</h2>
+    <form onSubmit={addAnecdote}>
       <div>
         <input name="anecdote" />
       </div>
-      <button type="submit">Create</button>
+      <button type="submit">create</button>
     </form>
   )
 }
