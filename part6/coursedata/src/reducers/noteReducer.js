cb69501d5f156b7e04f2cbc17e8076b0a3779013
Alejandro <aperalva@gmail.com>
2024-11-16 14:29:07 +0000

    segunda lectura parte c P6

--- part6/coursedata/src/reducers/noteReducer.js
+++ part6/coursedata/src/reducers/noteReducer.js
@@ -1,17 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id: 1,
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2,
-  },
-]
+import noteService from '../service/notes'
 
 const generateId = () =>
   Number((Math.random() * 1000000).toFixed(0))
@@ -21,9 +9,6 @@ const noteSlice = createSlice({
   name: 'notes',
   initialState: [],
   reducers: {
-    createNote(state, action) {
-      state.push(action.payload)
-    },
     toggleImportanceOf(state, action) {
       const id = action.payload
       const noteToChange = state.find(n => n.id === id)
@@ -44,5 +29,20 @@ const noteSlice = createSlice({
   },
 })
 
-export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+export const { toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+
+export const initializeNotes = () => {
+  return async dispatch =>  {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
+export const createNote = content => {
+  return async dispatch =>  {
+    const newNote = await noteService.createNote(content)
+    dispatch(appendNote(newNote))
+  }
+}
+
 export default noteSlice.reducer
