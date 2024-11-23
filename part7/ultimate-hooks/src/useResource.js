c1f5e26d2dc12cbd839fe1d0dccd085f6c0304a7
Alejandro <aperalva@gmail.com>
2024-11-23 10:01:28 +0000

    7.8: Hooks Definitivos

---
+++ part7/ultimate-hooks/src/useResource.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  useEffect(() => {
+    const fetchResources = async () => {
+      try {
+        const response = await axios.get(baseUrl)
+        setResources(response.data)
+      } catch (error) {
+        console.error('Error fetching resources:', error)
+      }
+    }
+
+    fetchResources()
+  }, [baseUrl])
+
+  const create = async (resource) => {
+    try {
+      const response = await axios.post(baseUrl, resource)
+      setResources((prevResources) => [...prevResources, response.data])
+    } catch (error) {
+      console.error('Error creating resource:', error)
+    }
+  }
+
+  const service = {
+    create,
+  }
+
+  return [resources, service]
+}
